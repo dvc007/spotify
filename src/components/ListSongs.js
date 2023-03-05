72a5fa034f60254aee31ada928d30311e72555d0
dvclearncode <dvchau2k@gmail.com>
2023-03-06 02:23:33 +0700

    update

--- src/components/ListSongs.js
+++ src/components/ListSongs.js
@@ -54,7 +54,7 @@ export default function ListSongs() {
                   <td className="px-6 py-4">{item.author}</td>
                   <td className="px-6 py-4">
                     <div className="flex items-center">
-                      <a href="/#">
+                      <a href={item.url}>
                         <button
                           type="button"
                           className="text-gray-900 bg-[#F7BE38] hover:bg-[#F7BE38]/90 focus:ring-4 focus:outline-none focus:ring-[#F7BE38]/50 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:focus:ring-[#F7BE38]/50 mr-2 mb-2"
